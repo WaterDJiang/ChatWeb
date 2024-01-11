bf870605ad4a4aca5f97d9e83aacd846dc0499af
WaterDJiang <121419758+WaterDJiang@users.noreply.github.com>
2024-01-11 19:43:00 +0800

    Update chatweb_page.py

--- chatweb_page.py
+++ chatweb_page.py
@@ -24,8 +24,7 @@ def show_chatweb_page():
         col1_1, col1_2 = st.columns([1, 4])
         # 主图像
         with col1_1:
-            current_dir = os.getcwd()
-            image_path = os.path.join(current_dir, 'images', 'im1.JPG')
+            image_path = "images/im1.JPG"
             st.image(image_path, width=70)
         # 网页标题
         with col1_2:
